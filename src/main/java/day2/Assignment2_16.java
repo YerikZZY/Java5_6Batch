2e6a98205637b8bb14751326c2d71ee9401c8e45
YerikZZY <YerikZZY@users.noreply.github.com>
2022-02-23 23:41:28 -0600

    assigment1 solution

--- src/main/java/day2/Assignment2_16.java
+++ src/main/java/day2/Assignment2_16.java
@@ -10,32 +10,64 @@ import java.util.stream.Collectors;
 
 public class Assignment2_16 {
 
-    private static long count(Criteria criteria) throws IOException {
-        long count=0;
-        List<Path> files = Files.list(criteria.folderPath).collect(Collectors.toList());
-        for(Path p : files) {
-            boolean isFolder = !(new File(String.valueOf(p)).isFile());
-            if(isFolder) {
-                count += count(new Criteria(p, true, 0));
+    private static Criteria count(Criteria criteria) throws IOException {
+        if(!criteria.isIncludeSubFolder()) {
+            String[] extension = new File(String.valueOf(criteria.getFolderPath())).getName().split("\\.");
+            if(criteria.getExtensionString().equals(extension[extension.length-1])) {
+                criteria.setExtensionCount(1);
             }
-            count++;
+            criteria.setFileCount(1);
+            return criteria;
         }
-        criteria.setFileCount(count);
+        long folderCount=1;
+        long fileCount=0;
+        long extensionCount=0;
+        List<Path> files = Files.list(criteria.getFolderPath()).collect(Collectors.toList());
+        for(Path p : files) {
+            Criteria c = new Criteria(p, !(new File(String.valueOf(p)).isFile()), criteria.getExtensionString());
+            folderCount += count(c).getFolderCount();
+            fileCount += count(c).getFileCount();
+            extensionCount += count(c).getExtensionCount();
+        }
+        criteria.setFolderCount(folderCount);
+        criteria.setFileCount(fileCount);
+        criteria.setExtensionCount(extensionCount);
 
-        return count;
+        return criteria;
     }
 
     public static void main(String[] args) {
+        if(args.length != 2) {
+            System.out.println("wrong input");
+            return;
+        }
         String pathString = args[0];
-        Criteria criteria = new Criteria();
-        criteria.setFolderPath(Paths.get(pathString));
-        long res = 0;
+        String extensionString = args[1];
+        Path path = Paths.get(pathString);
+        Criteria criteria = null;
         try {
-            res = count(criteria);
+            criteria = count(
+                    new Criteria(
+                        path,
+                        !(new File(String.valueOf(path)).isFile()),
+                        extensionString
+                    )
+                );
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(res);
+        if(criteria == null) {
+            System.out.println("wrong input");
+            return;
+        }
+        try {
+            System.out.println("There are");
+            System.out.println(count(criteria).getFolderCount() + " Folders");
+            System.out.println(count(criteria).getFileCount() + " Files");
+            System.out.println(count(criteria).getExtensionCount() + " Extensions");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/main/java/day2/Criteria.java
+++ src/main/java/day2/Criteria.java
@@ -4,17 +4,21 @@ import java.nio.file.Path;
 
 public class Criteria {
 
-    Path folderPath;
-    boolean includeSubFolder;
-    long fileCount;
+    private Path folderPath;
+    private boolean includeSubFolder;
+    private String extensionString;
+    private long folderCount;
+    private long fileCount;
+    private long extensionCount;
 
-    public Criteria() {
-    }
 
-    public Criteria(Path folderPath, boolean includeSubFolder, int fileCount) {
+    public Criteria(Path folderPath, boolean includeSubFolder, String extensionString) {
         this.folderPath = folderPath;
         this.includeSubFolder = includeSubFolder;
-        this.fileCount = fileCount;
+        this.extensionString = extensionString;
+        this.folderCount = 0;
+        this.fileCount = 0;
+        this.extensionCount = 0;
     }
 
     public Path getFolderPath() {
@@ -33,11 +37,35 @@ public class Criteria {
         this.includeSubFolder = includeSubFolder;
     }
 
+    public String getExtensionString() {
+        return extensionString;
+    }
+
+    public void setExtensionString(String extensionString) {
+        this.extensionString = extensionString;
+    }
+
+    public long getFolderCount() {
+        return folderCount;
+    }
+
+    public void setFolderCount(long folderCount) {
+        this.folderCount = folderCount;
+    }
+
     public long getFileCount() {
         return fileCount;
     }
 
     public void setFileCount(long fileCount) {
         this.fileCount = fileCount;
+    }
+
+    public long getExtensionCount() {
+        return extensionCount;
+    }
+
+    public void setExtensionCount(long extensionCount) {
+        this.extensionCount = extensionCount;
     }
 }
