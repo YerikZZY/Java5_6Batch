bc088ee1e398df1f1f373a1e61c6f53976842975
YerikZZY <YerikZZY@users.noreply.github.com>
2022-02-25 23:28:04 -0600

    update day4 assignment

--- src/main/java/day4/Assignment4_15.java
+++ src/main/java/day4/Assignment4_15.java
@@ -1,7 +1,7 @@
 package day4;
 
 public class Assignment4_15 {
-    // this assignment include 3 *.java file.
+    // this assignment include reader.java,Writer.java and A4.java file.
     public static void main(String[] args) {
 
     }
